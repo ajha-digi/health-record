6806e55f249c0d883aef268a4988ba8fa053a235
ajha-digi <ajha@digisprint.com>
2023-04-23 11:15:27 +0530

    Added dashbord to display all records.

---
+++ src/app/(home)/user/dashboard/page.js
@@ -0,0 +1,180 @@
+"use client"
+import { formatDate } from '@/helper';
+import { Form, InputNumber, Popconfirm, Table, Typography, Input } from 'antd';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const EditableCell = ({
+    editing,
+    dataIndex,
+    title,
+    inputType,
+    record,
+    index,
+    children,
+    ...restProps
+}) => {
+    console.log({dataIndex, title});
+    const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+    return (
+        <td {...restProps}>
+            {editing ? (
+                <Form.Item
+                    name={dataIndex}
+                    style={{
+                        margin: 0,
+                    }}
+                    rules={[
+                        {
+                            required: true,
+                            message: `Please Input ${title}!`,
+                        },
+                    ]}
+                >
+                    {inputNode}
+                </Form.Item>
+            ) : (
+                children
+            )}
+        </td>
+    );
+};
+
+const Dashboard = () => {
+    const [form] = Form.useForm();
+    const [data, setData] = useState([]);
+    const [editingKey, setEditingKey] = useState('');
+
+
+    useEffect(() => {
+        const getAllRecords = async () => {
+            const resp = await axios.get('/api/report')
+            setData(resp.data.data)
+        }
+        getAllRecords();
+    }, [])
+
+    const isEditing = (record) => record._id === editingKey;
+    const edit = (record) => {
+        form.setFieldsValue({
+            bp: '',
+            pulse: '',
+            weight: '',
+            date: '',
+            ...record,
+        });
+        setEditingKey(record._id);
+    };
+
+    const cancel = () => {
+        setEditingKey('');
+    };
+    const save = async (key) => {
+        try {
+            const row = await form.validateFields();
+            const newData = [...data];
+            const index = newData.findIndex((item) => key === item._id);
+            if (index > -1) {
+                const item = newData[index];
+                newData.splice(index, 1, {
+                    ...item,
+                    ...row,
+                });
+                await axios.patch(`/api/report?id=${item._id}`, row)
+                setData(newData);
+                setEditingKey('');
+            } else {
+                newData.push(row);
+                setData(newData);
+                setEditingKey('');
+            }
+        } catch (errInfo) {
+            console.log('Validate Failed:', errInfo);
+        }
+    };
+
+    const columns = [
+        {
+            title: 'Bp',
+            dataIndex: 'bp',
+            width: '15%',
+            editable: true,
+        },
+        {
+            title: 'Pulse',
+            dataIndex: 'pulse',
+            width: '5%',
+            editable: true,
+        },
+        {
+            title: 'Weight',
+            dataIndex: 'weight',
+            width: '10%',
+            editable: true,
+        },
+        {
+            title: 'Date',
+            dataIndex: 'createdAt',
+            width: '20%',
+            render: (_, record) => formatDate(record.createdAt)
+        },
+        {
+            title: 'operation',
+            dataIndex: 'operation',
+            render: (_, record) => {
+                const editable = isEditing(record);
+                return editable ? (
+                    <span>
+                        <Typography.Link
+                            onClick={() => save(record._id)}
+                            style={{
+                                marginRight: 8,
+                            }}
+                        >
+                            Save
+                        </Typography.Link>
+                        <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+                            <a>Cancel</a>
+                        </Popconfirm>
+                    </span>
+                ) : (
+                    <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+                        Edit
+                    </Typography.Link>
+                );
+            },
+        },
+    ];
+    const mergedColumns = columns.map((col) => {
+        if (!col.editable) {
+            return col;
+        }
+        return {
+            ...col,
+            onCell: (record) => ({
+                record,
+                inputType: col.dataIndex === 'bp' ? 'text' : 'number',
+                dataIndex: col.dataIndex,
+                title: col.title,
+                editing: isEditing(record),
+            }),
+        };
+    });
+    return (
+        <Form form={form} component={false}>
+            <Table
+                components={{
+                    body: {
+                        cell: EditableCell,
+                    },
+                }}
+                pagination={false} 
+                bordered
+                dataSource={data}
+                columns={mergedColumns}
+                rowClassName="editable-row"
+            />
+        </Form>
+    );
+};
+export default Dashboard;
--- src/helper/index.js
+++ src/helper/index.js
@@ -4,3 +4,12 @@ export const findId = async request => {
     const { id } = Object.fromEntries(urlSearchParams.entries());
     return id;
 }
+
+export const formatDate = (date) => {
+    const utcDate = new Date(date);
+    const ISTOffset = 330; // IST is UTC+5:30
+    const ISTDate = new Date(utcDate.getTime() + (ISTOffset * 60 * 1000));
+    const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+    const ISTDateString = ISTDate.toLocaleDateString('en-IN', options);
+    return ISTDateString;
+}
